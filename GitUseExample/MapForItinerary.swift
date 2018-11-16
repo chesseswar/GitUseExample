58b337b0ead9536fb3f9740036219859e0a15ee3
Eswaran, Pranav V <216430@s020rmc148-06.local>
2018-11-16 09:33:12 -0600

    make map feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Eswaran, Pranav V on 11/16/18.
+//  Copyright © 2018 Eswaran, Pranav V. All rights reserved.
+//
+
+import Foundation
